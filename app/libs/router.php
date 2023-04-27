d99b1811e4529a13ed8315d36b29bcf85140342e
Ulises Rendón <fdulises@outlook.com>
2023-04-27 00:22:02 -0600

    backup

--- app/libs/router.php
+++ app/libs/router.php
@@ -6,11 +6,11 @@ class router {
     
     private static array $routes = [];
 
-    public static function addRoute( string $url, string $controller, string $controller_method): void {
-        self::$routes[$url] = [$controller, $controller_method];
+    public static function addRoute( string $request_url, string $controller, string $controller_method): void {
+        self::$routes[$request_url] = [$controller, $controller_method];
     }
 
-    public static function route( string $url ): bool {
+    public static function route( string $request_url ): bool {
 
         foreach ( self::$routes as $routeUrl => $handler ) {
             list( $controllerName, $methodName ) = $handler;
@@ -20,7 +20,7 @@ class router {
                 $routeRegex = preg_replace( '/:([a-zA-Z0-9]+)/', '([^/]+)', $routeUrl );
                 $routeRegex = '/^' . str_replace( '/', '\/', $routeRegex ) . '$/';
 
-                if ( preg_match( $routeRegex, $url, $matches ) ) {
+                if ( preg_match( $routeRegex, $request_url, $matches ) ) {
                     // Remove the first match, which is the entire URL
                     array_shift( $matches );
 
@@ -29,7 +29,7 @@ class router {
                 }
             } else {
                 // If the route URL does not contain parameters, compare it directly to the URL
-                if ( $url === $routeUrl ) {
+                if ( $request_url === $routeUrl ) {
                     $controller = new $controllerName();
                     return $controller->$methodName();
                 }
